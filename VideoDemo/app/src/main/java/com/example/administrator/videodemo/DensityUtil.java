7ba2d02588ec444f79d223d19a01f2a2f427277d
AndDu <694495047@qq.com>
2019-04-11 14:17:36 +0800

    删除不要的文件

--- VideoDemo/app/src/main/java/com/example/administrator/videodemo/DensityUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.administrator.videodemo;
-
-import android.content.Context;
-
-class DensityUtil {
-    public static int dip2px(Context context, int dp) {
-        return (int) (context.getResources().getDisplayMetrics().density * dp + 0.5f);
-    }
-}
